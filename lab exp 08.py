61e8514806c008efbdf2a69ff1b7d0bed01dba54
LakshmiNarasimha9122 <leeladharratala997@gmail.com>
2026-08-19 11:29:20 +0530

    Update lab exp 08.py

--- lab exp 08.py
+++ lab exp 08.py
@@ -1,3 +1,6 @@
+#8. Scenario: You are a data scientist working for a company that sells products online. You have been  tasked with analyzing the sales data for the past month. The data is stored in a Pandas data frame. 
+#Question: How would you find the top 5 products that have been sold the most in the past month? 
+
 import pandas as pd
 
 # Sample DataFrame
