a58ff09d339b32c61560d6e3d9d6287dd146b0a9
LakshmiNarasimha9122 <leeladharratala997@gmail.com>
2026-08-19 11:40:33 +0530

    Update lab exp 02.py

--- lab exp 02.py
+++ lab exp 02.py
@@ -2,27 +2,15 @@
 #Question: How would you find the average price of all the products sold in the past month? Assume  3x3 matrix with each row representing the sales for a different product
 
 import numpy as np
+import pandas as pd
 
-rows = int(input("Enter number of products: "))
-columns = int(input("Enter number of sales prices for each product: "))
+data = pd.read_csv("sales_data.csv")
 
-sales_data = []
-
-for i in range(rows):
-    print(f"\nEnter sales prices for Product {i + 1}:")
-    product_sales = []
-
-    for j in range(columns):
-        price = float(input(f"Price {j + 1}: "))
-        product_sales.append(price)
-
-    sales_data.append(product_sales)
-
-sales_data = np.array(sales_data)
+sales_data = data.to_numpy()
 
 average_price = np.mean(sales_data)
 
-print("\nSales Data:")
+print("Sales Data:")
 print(sales_data)
 
-print("\nAverage Price of All Products Sold =", average_price)
+print("\nAverage Price of All Products Sold =", round(average_price, 2))
