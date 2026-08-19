a8c00f4b56a1e840a8b41d7c3ee9a32fc96eb904
LakshmiNarasimha9122 <leeladharratala997@gmail.com>
2026-08-19 11:48:01 +0530

    Update lab exp 08.py

--- lab exp 08.py
+++ lab exp 08.py
@@ -3,17 +3,9 @@
 
 import pandas as pd
 
-# Sample DataFrame
-sales_data = pd.DataFrame({
-    'Product_Name': ['Laptop', 'Mouse', 'Keyboard', 'Laptop',
-                     'Mouse', 'Laptop', 'Keyboard', 'Monitor'],
-    'Quantity_Sold': [5, 10, 8, 7, 6, 4, 9, 3]
-})
+df = pd.read_csv("sales_data.csv")
 
-# Find Top 5 Most Sold Products
-top5_products = sales_data.groupby('Product_Name')['Quantity_Sold'].sum() \
-                          .sort_values(ascending=False) \
-                          .head(5)
+top5 = df.groupby("Product_Name")["Quantity_Sold"].sum().sort_values(ascending=False).head(5)
 
 print("Top 5 Most Sold Products:")
-print(top5_products)
+print(top5)
